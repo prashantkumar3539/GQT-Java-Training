c4d388ddb4e9271afa8748127f441af8dfc7bcb4
Prashant Kumar <Prashant Kumar@Laptop>
2023-03-23 07:08:07 +0530

    Second Commit

--- src/GqtCodes/Program1.java
+++ src/GqtCodes/Program1.java
@@ -5,6 +5,7 @@ public class Program1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Prashant !!");
+		System.out.println("Hello Prashant Kumar");
 
 	}
 
